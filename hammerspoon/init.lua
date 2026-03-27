2eb9ed192a3ed5c2d3359def16c6adca3a8a7677
Ella Dunbar <git@elladunbar.com>
2026-03-27 12:15:55 -0500

    Add hammerspoon

---
+++ hammerspoon/init.lua
@@ -0,0 +1,62 @@
+-- scrolling window manager
+PaperWM = hs.loadSpoon("PaperWM")
+PaperWM:bindHotkeys({
+	focus_left = { { "alt" }, "h" },
+	focus_right = { { "alt" }, "l" },
+	focus_up = { { "alt" }, "k" },
+	focus_down = { { "alt" }, "j" },
+
+	swap_left = { { "alt", "shift" }, "h" },
+	swap_right = { { "alt", "shift" }, "l" },
+	swap_up = { { "alt", "shift" }, "k" },
+	swap_down = { { "alt", "shift" }, "j" },
+
+	switch_space_1 = { { "alt" }, "1" },
+	switch_space_2 = { { "alt" }, "2" },
+	switch_space_3 = { { "alt" }, "3" },
+	switch_space_4 = { { "alt" }, "4" },
+	switch_space_5 = { { "alt" }, "5" },
+	switch_space_6 = { { "alt" }, "6" },
+	switch_space_7 = { { "alt" }, "7" },
+	switch_space_8 = { { "alt" }, "8" },
+	switch_space_9 = { { "alt" }, "9" },
+
+	move_window_1 = { { "alt", "shift" }, "1" },
+	move_window_2 = { { "alt", "shift" }, "2" },
+	move_window_3 = { { "alt", "shift" }, "3" },
+	move_window_4 = { { "alt", "shift" }, "4" },
+	move_window_5 = { { "alt", "shift" }, "5" },
+	move_window_6 = { { "alt", "shift" }, "6" },
+	move_window_7 = { { "alt", "shift" }, "7" },
+	move_window_8 = { { "alt", "shift" }, "8" },
+	move_window_9 = { { "alt", "shift" }, "9" },
+
+	increase_width = { { "alt", "ctrl" }, "l" },
+	decrease_width = { { "alt", "ctrl" }, "h" },
+
+	toggle_floating = { { "alt" }, "d" },
+})
+PaperWM.window_gap = { top = 0, bottom = 20, left = 20, right = 20 }
+PaperWM.swipe_fingers = 3
+PaperWM.swipe_gain = 2.0
+PaperWM:start()
+
+-- app hotkeys
+hs.hotkey.bind({ "alt" }, "e", function()
+	hs.application.launchOrFocus("Finder")
+end)
+
+-- play video with clipboard content
+hs.hotkey.bind({ "alt" }, "v", function()
+	local cmd = "~/Code/sh/mpv/play.sh " .. hs.pasteboard.getContents()
+	os.execute(cmd)
+end)
+
+-- reload config
+hs.hotkey.bind({ "alt" }, "r", function()
+	if PaperWM then
+		PaperWM:stop()
+	end
+	hs.reload()
+end)
+hs.alert.show("Reloaded Hammerspoon config!")
