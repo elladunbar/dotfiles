319de622b176cce365f7a5011d8167ba45029cdf
Ella Dunbar <git@elladunbar.com>
2026-08-09 08:31:12 -0500

    Use new eza option syntax in fish config

--- fish/config.fish
+++ fish/config.fish
@@ -19,12 +19,12 @@ if status is-interactive
     # file management
     abbr --add ,cpdirs rsync --archive --include='*/' --exclude='*'
     abbr --add ,rs rsync --archive --compress --human-readable --progress
-    abbr --add la eza --all --hyperlink
-    abbr --add ll eza --all --long --icons=auto --git --hyperlink
-    abbr --add ls eza --hyperlink
-    abbr --add tree eza --tree --icons=auto --hyperlink
+    abbr --add la eza --all --hyperlink=auto
+    abbr --add ll eza --all --long --icons=auto --git --hyperlink=auto
+    abbr --add ls eza --hyperlink=auto
+    abbr --add tree eza --tree --icons=auto --hyperlink=auto
     function ,cl
-        cd $argv[1]; and eza --hyperlink
+        cd $argv[1]; and eza --hyperlink=auto
     end
     function ,mkcd
         mkdir -p $argv[1]; and cd $argv[1]
